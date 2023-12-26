c4e5ab1e32a0027fb4d078b92289e6cf3d1281a3
melelong <1137042726@qq.com>
2023-12-26 22:33:37 +0800

    build(custom): 集成eslint,prettier和lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,40 +1,37 @@
-// 引入 ESLint 模块，它提供了一个 ESLint 类来配置和执行 ESLint 操作
+// 导入 ESLint 包，以便使用 ESLint 类对文件执行代码质量检查
 const { ESLint } = require('eslint');
 
 /**
- * 过滤掉 ESLint 配置中被忽略的文件。
- * 
- * @param {string[]} files - 一个待检查文件路径的数组。
- * @returns {Promise<string>} 返回一个承诺，解析为一个不包含被忽略文件的字符串，
- * 文件路径之间用空格分隔。
+ * 从提供的文件数组中移除那些被 ESLint 配置忽略的文件。
+ *
+ * @param {string[]} files - 一个包含文件路径的字符串数组。
+ * @returns {Promise<string>} 返回一个字符串，其中包含所有未被 ESLint 忽略的文件路径，这些路径用空格分隔。
  */
 const removeIgnoredFiles = async (files) => {
-  console.log('执行');
-  // 创建一个 ESLint 实例
+  // 实例化 ESLint
   const eslint = new ESLint();
 
-  // 使用 map 函数对每个文件路径执行 isPathIgnored 方法，该方法检查文件是否被 ESLint 忽略
-  // 并将结果包装在 Promise.all 中，以并行处理所有的检查
+  // 使用 map 方法对每个文件路径调用 isPathIgnored 方法来确定其是否被 ESLint 忽略
+  // Promise.all 等待所有的异步检查操作完成
   const ignoredFiles = await Promise.all(files.map((file) => eslint.isPathIgnored(file)));
 
-  // 使用 filter 方法移除那些被 ESLint 忽略的文件，ignoredFiles 数组的相应位置将为 true
+  // 使用 filter 方法创建一个新的数组，该数组只包含不被 ESLint 忽略的文件
   const filteredFiles = files.filter((_, i) => !ignoredFiles[i]);
 
-  // 将剩余的文件路径连接为一个单一的字符串，文件路径之间用空格分隔
+  // 将过滤后的文件路径数组转换为一个用空格分隔的字符串，以便作为命令行参数使用
   return filteredFiles.join(' ');
 };
 
-// 导出一个配置对象，它将被 lint-staged 使用，lint-staged 允许我们对 git 暂存区的文件执行命令
+// 导出一个配置对象，它将被 lint-staged 使用，lint-staged 允许我们在 git 提交之前对暂存的文件运行脚本
 module.exports = {
-  // 这里配置了一个针对所有文件的 lint 任务
+  // 这里特别针对 JavaScript 和 TypeScript 文件，包括 JSX 和 TSX 变体
   '*.{js,jsx,ts,tsx}': async (files) => {
-    // 首先过滤掉 ESLint 配置中被忽略的文件
-    const res = await removeIgnoredFiles(files)
-    console.log(res);
-    const filesToLint = res;
+    // 首先移除被 ESLint 忽略的文件
+    const filesToLint = await removeIgnoredFiles(files);
 
-    // 返回一个数组，包含了要执行的 ESLint 命令
-    // --max-warnings=0 标志将导致即使只有警告，也将退出状态码设置为错误，这可以强制开发者解决所有的警告
-    return [`npx eslint ${filesToLint} --max-warnings=0 --fix`, `npx prettier -w ${filesToLint}`];
+    // 返回一个命令数组，其中包含了调用 ESLint 和 Prettier 的命令
+    // npx eslint ... --fix 参数会自动修复可以修复的问题
+    // npx prettier -w ... 将格式化代码，-w 代表写入更改
+    return [`npx eslint ${filesToLint} --max-warnings=0 --fix`, `npx prettier --config .prettierrc . -w`];
   },
 };
