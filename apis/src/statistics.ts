a2715458736a77ac15394755d7bd28e8c30bdd3a
melelong <1137042726@qq.com>
2024-01-02 17:38:31 +0800

    '换模板'

--- apis/src/statistics.ts
+++
@@ -1,8 +0,0 @@
-import api from './api';
-// 统计模块
-export const statisticsApi = {
-  // 	会议室使用频率统计
-  meeting_room_usage_frequency: () => api.get('/api/banner'),
-  // 	用户预订频率统计
-  user_booking_frequency: () => api.get('/api/user/register'),
-};
--- utils/src/crypto.ts
+++
@@ -1,8 +0,0 @@
-import CryptoJS from 'crypto-js';
-
-function md5(str: string) {
-  return CryptoJS.MD5(str).toString(CryptoJS.enc.Hex);
-}
-export default {
-  md5,
-};
